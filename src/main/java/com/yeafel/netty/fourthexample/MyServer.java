a37ecd16ac22d3d3edcf1911fa1a62b3ead24fb8
yeafel <yeafel@qq.com>
2019-07-24 23:41:06 +0800

    netty的心跳检测

---
+++ src/main/java/com/yeafel/netty/fourthexample/MyServer.java
@@ -0,0 +1,36 @@
+package com.yeafel.netty.fourthexample;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+/**
+ * @author Yeafel
+ * 2019/7/24 22:50
+ * 本类中handler是对于bossGroup来说的，childHandler是对于workerGroup来说的
+ * Do or Die,To be a better man!
+ */
+public class MyServer {
+
+    public static void main(String[] args) throws Exception{
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class).
+                    handler(new LoggingHandler(LogLevel.INFO)).
+                    childHandler(new MyServerInitializer());
+
+            ChannelFuture channelFuture = serverBootstrap.bind(8899).sync();
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
---
+++ src/main/java/com/yeafel/netty/fourthexample/MyServerInitializer.java
@@ -0,0 +1,24 @@
+package com.yeafel.netty.fourthexample;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.timeout.IdleStateHandler;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Yeafel
+ * 2019/7/24 22:57
+ * Do or Die,To be a better man!
+ */
+public class MyServerInitializer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+
+        pipeline.addLast(new IdleStateHandler(5,7,10, TimeUnit.SECONDS));
+        pipeline.addLast(new MyServerHandler());
+    }
+}
