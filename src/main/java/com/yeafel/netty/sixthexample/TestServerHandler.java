f8a1adb3128b8c9be6ef8fdaa4132bd89dd98d3b
yeafel <yeafel@qq.com>
2019-07-28 21:08:32 +0800

    protobuf定义的外部类里面有多种对象，让netty选择性的接收某个对象。

--- src/main/java/com/yeafel/netty/sixthexample/TestServerHandler.java
+++ src/main/java/com/yeafel/netty/sixthexample/TestServerHandler.java
@@ -8,12 +8,28 @@ import io.netty.channel.SimpleChannelInboundHandler;
  * 2019/7/28 15:57
  * Do or Die,To be a better man!
  */
-public class TestServerHandler extends SimpleChannelInboundHandler<MyDataInfo.Person> {
+public class TestServerHandler extends SimpleChannelInboundHandler<MyDataInfo.MyMessage> {
 
     @Override
-    protected void channelRead0(ChannelHandlerContext ctx, MyDataInfo.Person msg) throws Exception {
-        System.out.println(msg.getName());
-        System.out.println(msg.getAge());
-        System.out.println(msg.getAddress());
+    protected void channelRead0(ChannelHandlerContext ctx, MyDataInfo.MyMessage msg) throws Exception {
+        MyDataInfo.MyMessage.DataType dataType = msg.getDataType();
+
+        if (dataType == MyDataInfo.MyMessage.DataType.PersonType) {
+            MyDataInfo.Person person = msg.getPerson();
+            System.out.println(person.getName());
+            System.out.println(person.getAge());
+            System.out.println(person.getAddress());
+
+        } else if (dataType == MyDataInfo.MyMessage.DataType.DogType) {
+            MyDataInfo.Dog dog = msg.getDog();
+            System.out.println(dog.getName());
+            System.out.println(dog.getAge());
+        } else {
+            MyDataInfo.Cat cat = msg.getCat();
+            System.out.println(cat.getName());
+            System.out.println(cat.getCity());
+        }
+
+
     }
 }
